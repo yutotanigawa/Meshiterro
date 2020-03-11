65196c50183c4b5f80d63a867bf0dc7f043c28d2
yutotanigawa <61495631+yutotanigawa@users.noreply.github.com>
2020-03-11 18:43:09 +0900

    [Start]5-5

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -20,6 +20,11 @@ class PostImagesController < ApplicationController
 		@post_image = PostImage.find(params[:id])
 		@post_comment = PostComment.new
 	end
+	def destroy
+		@post_image = PostImage.find(params[:id])
+		@post_image.destroy
+		redirect_to post_imagges_path
+	end
 
 private
 	def post_image_params
