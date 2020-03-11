d44538d4d9eba5edf5df2eeef9549f57e08729aa
yutotanigawa <61495631+yutotanigawa@users.noreply.github.com>
2020-03-11 15:24:07 +0900

    [Finish]5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'fa99ddb68de9112d1da2161d32dde9bd9eb90395ce2e53e185b16edefbc9054a074f45176be90adba69664ab94d87ee8866a23657f6c5ea0b0a85d6e8add4884'
