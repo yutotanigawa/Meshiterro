e6be4cea7ab9029209e02d423b3ca6e1f76807ac
yutotanigawa <61495631+yutotanigawa@users.noreply.github.com>
2020-03-11 20:52:55 +0900

    [Finish]5-6

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_11_080754) do
+ActiveRecord::Schema.define(version: 2020_03_11_111732) do
 
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2020_03_11_080754) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "profile_image_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
